6eeb544014ad04a0baa2fa58338ee19d237a2eb5
cannin <cannin@gmail.com>
2017-08-23 21:12:16 -0400

    Added tm Removed local installPackages

--- runInstallPackages.R
+++ runInstallPackages.R
@@ -3,9 +3,9 @@ setRepositories(ind=1:6)
 options(repos="http://cran.rstudio.com/")
 if(!require(devtools)) { install.packages("devtools") }
 
-source("installPackages.R")
+source("https://gist.githubusercontent.com/cannin/6b8c68e7db19c4902459/raw/installPackages.R")
 
-# Install packages 
+# Install packages
 pkg <- read.table("r-requirements.txt", stringsAsFactors = FALSE)
 pkg <- pkg$V1
 
